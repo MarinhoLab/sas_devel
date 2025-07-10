f429656907d8d8498ca4973a63c27c0105fbfae1
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-10 09:37:03 +0100

    Adding sas_devel folder and setup_environment script

---
+++ sas_devel/scripts/setup_environment.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+####################################################################
+#                        Make src directory
+####################################################################
+
+mkdir -p src
+cd src
+
+####################################################################
+#                        Array of packages
+####################################################################
+
+# Packages in https://github.com/SmartArmStack/
+sas_pkg_array=(
+"sas_core"
+"sas_msgs"
+"sas_common"
+"sas_conversions"
+"sas_datalogger"
+"sas_robot_driver"
+"sas_robot_kinematics"
+"sas_robot_driver_denso"
+)
+
+# Packages in https://github.com/MarinhoLab/
+marinholab_pkg_array=(
+"sas_robot_driver_kuka"
+"sas_robot_driver_ur"
+)
+
+####################################################################
+#                        Clone all packages
+####################################################################
+
+echo "Cloning packages at SmartArmStack"
+for pkg_name in "${sas_pkg_array[@]}"; do
+  echo "Cloning ${pkg_name}"
+  git clone -b "$rosv" git@github.com:SmartArmStack/"$pkg_name".git --recurse-submodules
+done
+
+echo "Cloning packages at MarinhoLab"
+for pkg_name in "${marinholab_pkg_array[@]}"; do
+  echo "Cloning ${pkg_name}"
+  git clone -b "$rosv" git@github.com:MarinhoLab/"$pkg_name".git --recurse-submodules
+done
+
+####################################################################
+#                        Build and add to source
+####################################################################
+
+colcon build
+
+echo "source $HOME/sas_devel/install/setup.bash"\
+>> /etc/bash_env
+
+
